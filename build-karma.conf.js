deed40b3102e1b43273a661d6a46b1140f26284a
Uwe Klosa <uwe.klosa@ub.uu.se>
2016-02-11 11:16:08 +0100

    CORA-131: changed karma config

--- build-karma.conf.js
+++ build-karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function (config)
 
         coverageReporter: {
             type: 'lcov',
-            dir: 'target/karma-coverage'
+            dir: 'karma-coverage'
         },
 
         junitReporter: {
