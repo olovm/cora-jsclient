c7a8d6922eabef49b783f6adaacde14565967dc7
olov <olov.mckie@ub.uu.se>
2016-11-24 09:23:06 +0100

    fix upload not working

--- src/main/webapp/script/jsClient.js
+++ src/main/webapp/script/jsClient.js
@@ -33,6 +33,8 @@ var CORA = (function(cora) {
 		function start() {
 			mainView.appendChild(busy.getView());
 			var uploadManagerSpec = spec.dependencies;
+
+			uploadManagerSpec.dependencies = spec.dependencies;
 			uploadManagerSpec.jsClient = out;
 
 			var recordGuiFactorySpec = spec.dependencies;
