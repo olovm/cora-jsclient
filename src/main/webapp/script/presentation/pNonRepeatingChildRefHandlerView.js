1478cbab872684ee37c1800bfe3f0803738ca0a7
olov <olov@5d707a4cbe3c>
2018-01-05 10:24:05 +0000

    fixing sonar complaint

--- src/main/webapp/script/presentation/pNonRepeatingChildRefHandlerView.js
+++ src/main/webapp/script/presentation/pNonRepeatingChildRefHandlerView.js
@@ -25,21 +25,6 @@ var CORA = (function(cora) {
 			view = createBaseView();
 
 		}
-		// var childrenView = createChildrenView();
-		// var buttonView;
-		//
-		// var nodeBeeingDragged;
-		// var lastChangedWith;
-		// var addDragged;
-		// var beeingDraggedY;
-		// var childIsCurrentlyBeeingDragged = false;
-		// var lastRepeatingElementDraggedOver;
-		//
-		// view.appendChild(childrenView);
-		// if (spec.mode==="input" && (spec.addMethod !== undefined || spec.upload === "true")) {
-		// createButtonView();
-		// }
-		//
 		function createBaseView() {
 			var newClassName = "pNonRepeatingChildRefHandler";
 			if (spec.textStyle !== undefined) {
@@ -55,122 +40,17 @@ var CORA = (function(cora) {
 		function getView() {
 			return view;
 		}
-		//
-		// function createButtonView() {
-		// var buttonViewNew = CORA.gui.createSpanWithClassName("buttonView");
-		// if (spec.upload !== "true") {
-		// buttonViewNew.appendChild(createAddButton());
-		// } else {
-		// buttonViewNew.appendChild(createBrowseButton());
-		//
-		// }
-		// buttonView = buttonViewNew;
-		// view.appendChild(buttonView);
-		// }
-		//
-		// function createAddButton() {
-		// var button = document.createElement("input");
-		// button.type = "button";
-		// button.value = spec.addText;
-		// button.onclick = spec.addMethod;
-		// return button;
-		// }
-		//
-		// function createBrowseButton() {
-		// var button = document.createElement("input");
-		// button.type = "file";
-		// button.multiple = "true";
-		// button.onchange = function() {
-		// spec.handleFilesMethod(this.files);
-		// };
-		// return button;
-		// }
-		//
-		// function hideButtonView() {
-		// buttonView.styleOriginal = buttonView.style.display;
-		// buttonView.style.display = "none";
-		// }
-		//
-		// function showButtonView() {
-		// if (buttonView.styleOriginal !== undefined) {
-		// buttonView.style.display = buttonView.styleOriginal;
-		// }
-		// }
 
 		function addChild(child) {
 			view.appendChild(child);
 		}
 
-		// function removeChild(child) {
-		// childrenView.removeChild(child);
-		// }
-		//
-		// function moveChild(dataFromMsg) {
-		// var childToMove = findRepeatingElementByPath(dataFromMsg.moveChild);
-		// var basePositionOnChild = findRepeatingElementByPath(dataFromMsg.basePositionOnChild);
-		//
-		// if (dataFromMsg.newPosition === "after") {
-		// childrenView.insertBefore(childToMove, basePositionOnChild.nextSibling);
-		// } else {
-		// childrenView.insertBefore(childToMove, basePositionOnChild);
-		// }
-		// }
-		//
-		// function findRepeatingElementByPath(pathToFind) {
-		// var repeatingElements = childrenView.childNodes;
-		// var jsonPathToFind = JSON.stringify(pathToFind);
-		// var childKeys = Object.keys(childrenView.childNodes);
-		// var foundKey = childKeys.find(function(repeatingElementKey) {
-		// var repeatingElement = repeatingElements[repeatingElementKey];
-		// var jsonPath = JSON.stringify(repeatingElement.modelObject.getPath());
-		// return jsonPathToFind === jsonPath;
-		// });
-		// return repeatingElements[foundKey];
-		// }
-		//
-		// function hideChildrensRemoveButton() {
-		// callOnceOnEachRepeatingElement("hideRemoveButton");
-		// }
-		//
-		// function callOnceOnEachRepeatingElement(functionToRun) {
-		// var repeatingElements = childrenView.childNodes;
-		// var repeatingElementsKeys = Object.keys(repeatingElements);
-		// repeatingElementsKeys.forEach(function(key) {
-		// repeatingElements[key].modelObject[functionToRun]();
-		// });
-		// }
-		//
-		// function showChildrensRemoveButton() {
-		// callOnceOnEachRepeatingElement("showRemoveButton");
-		// }
-		//
-		// function hideChildrensDragButton() {
-		// callOnceOnEachRepeatingElement("hideDragButton");
-		// }
-		//
-		// function showChildrensDragButton() {
-		// callOnceOnEachRepeatingElement("showDragButton");
-		// }
-		//
-		// function getSpec() {
-		// return spec;
-		// }
-		//
-		// function getDependencies() {
-		// return dependencies;
-		// }
 
 		var out = Object.freeze({
 			"type" : "pNonRepeatingChildRefHandlerView",
-			// getSpec : getSpec,
-			// getDependencies : getDependencies,
 			getView : getView,
-			// setRepeatingElementDragOver : setRepeatingElementDragOver,
-			addChild : addChild,
-		// hideButtonView : hideButtonView,
-		// showButtonView : showButtonView
+			addChild : addChild
 		});
-		// view.viewObject = out;
 		start();
 		return out;
 	};
