bca89a5421e94e20a054952ef0619a56b80a578f
Johan Andersson <johan.andersson@ub.uu.se>
2020-01-29 13:50:59 +0100

    Removed firefox headless tests karma

--- karma.conf.js
+++ karma.conf.js
@@ -23,6 +23,6 @@ module.exports = function(config) {
 	config.set(karma_config({
 		singleRun : true,
 		reporters : [ 'dots', 'junit', 'coverage' ],
-		browsers : [ 'FirefoxHeadless', 'ChromiumHeadless' ]
+		browsers : [ 'ChromiumHeadless' ]
 	}));
 };
